ae15820d15dc4f7f33449ad043dd9ee95ac6fd70
AdityacodeNIT <srivastavaditya145@gmail.com>
2024-12-06 13:39:21 +0530

    newspush

---
+++ src/routes/news.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { addNews, getNews } from "../controllers/news.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
+const newsrouter = Router();
+
+// Route to add news
+newsrouter.post(
+  "/addNews",
+  upload.fields([
+    { name: "images", maxCount: 10 },
+    { name: "paragraphImages", maxCount: 10 },
+  ]),
+  verifyJWT,
+  addNews
+);
+
+// Route to get news
+newsrouter.get("/getNews", getNews);
+
+export default newsrouter;
